03662efaa6042a3c445333928f676b2bd93bc1c0
camrynwhisenant <90424311+camrynwhisenant@users.noreply.github.com>
2021-11-20 12:02:57 -0700

    templateinheritance

--- TaskEditor/views.py
+++ TaskEditor/views.py
@@ -3,19 +3,25 @@ from django.http import HttpResponse
 
 # Create your views here.
 def indexPageView(request) :
-    return HttpResponse("Welcome to TaskList!")
+    return render(request, 'taskeditor/index.html')
+    #return HttpResponse("WelcomeToTaskList")
 
 def aboutPageView(request) :
-    return HttpResponse("This program will help you manage all the school tasks that are keeping you busy and stressed.")
+    return render(request, 'taskeditor/about.html')
+    #return HttpResponse("This program will help you manage all the school tasks that are keeping you busy and stressed.")
 
 def seeTaskPageView(request) :
-    return HttpResponse("This page will be where your tasks are displayed")
+    return render(request, 'taskeditor/seetask.html')
+    #return HttpResponse("This page will be where your tasks are displayed")
 
 def createTaskPageView(request) :
-    return HttpResponse("This page will be where you create a new task")
+    return render(request, 'taskeditor/createtask.html')
+    #return HttpResponse("This page will be where you create a new task")
 
 def editTaskPageView(request) :
-    return HttpResponse("This page will be where you edit an exisiting task")
+    return render(request, 'taskeditor/edittask.html')
+    #return HttpResponse("This page will be where you edit an exisiting task")
 
 def deleteTaskPageView(request) :
-    return HttpResponse("This page will be where you delete an exisiting task")
+    return render(request, 'taskeditor/deletetask.html')
+    #return HttpResponse("This page will be where you delete an exisiting task")
