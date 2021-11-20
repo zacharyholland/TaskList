048cb851cde861515fe04a40e706d9a776a80e23
camrynwhisenant <90424311+camrynwhisenant@users.noreply.github.com>
2021-11-20 14:37:58 -0700

    settings(models)

--- TaskList/settings.py
+++ TaskList/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'TaskList.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'travel',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost'
+        }
+    }       
 
 
 # Password validation
