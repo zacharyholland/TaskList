4fc2e4ecaf86f77f662e3ccbda742a7033922e9e
camrynwhisenant <90424311+camrynwhisenant@users.noreply.github.com>
2021-11-20 14:45:02 -0700

    namedatabase

--- TaskEditor/models.py
+++ TaskEditor/models.py
@@ -26,30 +26,12 @@ class Priority(models.Model) :
     def __str__(self) :
         return (self.priority)
 
-class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    user_name = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    email = models.EmailField(max_length=100)
 
-    def __str__(self):
-        return (self.full_name)
-
-    @property
-    def full_name(self):
-            return '%s %s' % (self.first_name, self.last_name)
-
-    def save(self):
-        self.first_name = self.first_name.upper()
-        self.last_name = self.last_name.upper()
-        super(User, self).save()
 
 class Task(models.Model) :
     course = models.OneToManyField(ClassList, models.CASCADE)
     assignment = models.OneToMany(AssignmentType, models.CASCADE)
     priority = models.OneToMany(Priority, models.CASCADE)
-    user = models.OneToManyField(User, models.CASCADE)
     title = models.CharField(max_length=50)
     due_date = models.DateField(default=datetime.today, blank=True)
     completed = BooleanField(default=False)
--- TaskList/settings.py
+++ TaskList/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'TaskList.wsgi.application'
 DATABASES = {
         'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'travel',
+        'NAME': 'Tasklist',
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': 'localhost'
