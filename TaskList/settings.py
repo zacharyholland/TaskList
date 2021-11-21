79c9f200c204ee4ec813391bfae88faaa12cad73
camrynwhisenant <90424311+camrynwhisenant@users.noreply.github.com>
2021-11-20 23:01:49 -0700

    removepass

--- TaskList/settings.py
+++ TaskList/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'TaskList',
         'USER': 'postgres',
-        'PASSWORD': 'camryn99',
+        'PASSWORD': '',
         'HOST': 'localhost'
         }
     }       
